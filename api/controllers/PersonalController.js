cdf4c6ae8bf9d9072d06d737f6cf9213ab89fc39
joseluisseg05 <joseluistimy@gmail.com>
2021-04-23 19:08:30 -0500

    estructura

--- api/controllers/PersonalController.js
+++ api/controllers/PersonalController.js
@@ -4,135 +4,137 @@
  * @description :: Server-side actions for handling incoming requests.
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
-var Response = require('../common/Response');
+var Response = require("../common/Response");
 var Response = new Response();
 
-const Personal = require('../bl/PersonalBl');
+const Personal = require("../bl/PersonalBl");
 
 module.exports = {
     login: async (req, res) => {
         const personal = req.body.personal;
         Personal.Login(personal)
-        .then( data => {
+        .then((data) => {
             const response = Response.successResponse();
             response.data = data;
             return res.send(response);
         })
-        .catch( error => {
+        .catch((error) => {
             const response = Response.errorResponse();
             response.error.message = error.message;
             response.error.code = 400;
-            return res.status(400).send(response);//quitar status??
-        })
+            return res.status(400).send(response); //quitar status??
+        });
     },
 
     confirmar: async (req, res) => {
         Personal.Confirmar(req)
-        .then( data => {
+        .then((data) => {
             const response = Response.successResponse();
             response.data = data;
             return res.send(response);
         })
-        .catch( error => {
+        .catch((error) => {
             const response = Response.errorResponse();
             response.error.message = error.message;
             response.error.code = 400;
-            return res.status(400).send(response);//quitar status??
-        })
+            return res.status(400).send(response); //quitar status??
+        });
     },
 
-  create: async(req, res) => {
-    const personal = req.body.personal;
-    Personal.Create(personal, req)
-    .then( data => {
-        const response = Response.successResponse();
-        response.data = data;
-        return res.status(201).send(response);
-    }) 
-    .catch( error => {
-        const response = Response.errorResponse();
-        response.error.message = error.message;
-        response.error.code = 400;
-        return res.status(400).send(response)
-    })
-  },
-  update: async (req, res) => {
-    const personal = req.body.personal;
-    Personal.Update(personal)
-    .then( data => {
-        const response = Response.successResponse();
-        response.data = data;
-        return res.status(200).send(response);
-    })
-    .catch( error => {
-        const response = Response.errorResponse();
-        response.error.message = error.message;
-        response.error.code = 400;
-        return res.status(400).send(response);
-    })
-  },
-  find: async (req, res) => {
-    const filter = req.body.filter;
-    Personal.Find(filter)
-    .then( data => {
-        const response = Response.successResponse();
-        response.data = data;
-        return res.send(response);
-    })
-    .catch( error => {
-        const response = Response.errorResponse();
-        response.error.message = error.message;
-        response.error.code = 400;
-        return res.status(400).send(response);//quitar status??
-    })
-},
+    create: async (req, res) => {
+        const personal = req.body.personal;
+        Personal.Create(personal, req)
+        .then((data) => {
+            const response = Response.successResponse();
+            response.data = data;
+            return res.status(201).send(response);
+        })
+        .catch((error) => {
+            const response = Response.errorResponse();
+            response.error.message = error.message;
+            response.error.code = 400;
+            return res.status(400).send(response);
+        });
+    },
 
-  pagination: async (req, res) => {
-    const {limit= 5, skip= 0} = req.query;
-    //console.log(req.user) una vez que inician sesion ya se obtiene los datos del usuario con req.user
-    Personal.Pagination(limit, skip)
-    .then( data => {
-        const response = Response.successResponse();
-        response.data = data;
-        return res.send(response);
-    })
-    .catch( error => {
-        const response = Response.errorResponse();
-        response.error.message = error.message;
-        response.error.code = 400;
-        return res.status(400).send(response);//quitar status??
-    })
-  },
-  deactivate: async (req, res) => {
-    const id_personal = req.params.id_personal;
-    Personal.Deactivate(id_personal)
-    .then( data => {
-        const response = Response.successResponse();
-        response.data = data;
-        return res.send(response);
-    })
-    .catch( error => {
-        const response = Response.errorResponse();
-        response.error.message = error.message;
-        response.error.code = 400;
-        return res.status(400).send(response);//quitar status??
-    })
-},
+    update: async (req, res) => {
+        const personal = req.body.personal;
+        Personal.Update(personal)
+        .then((data) => {
+            const response = Response.successResponse();
+            response.data = data;
+            return res.status(200).send(response);
+        })
+        .catch((error) => {
+            const response = Response.errorResponse();
+            response.error.message = error.message;
+            response.error.code = 400;
+            return res.status(400).send(response);
+        });
+    },
 
-activate: async (req, res) => {
-    const id_personal = req.params.id_personal;
-    Personal.Activate(id_personal)
-    .then( data => {
-        const response = Response.successResponse();
-        response.data = data;
-        return res.send(response);
-    })
-    .catch( error => {
-        const response = Response.errorResponse();
-        response.error.message = error.message;
-        response.error.code = 400;
-        return res.status(400).send(response);//quitar status??
-    })
-},
+    find: async (req, res) => {
+        const filter = req.body.filter;
+        Personal.Find(filter)
+        .then((data) => {
+            const response = Response.successResponse();
+            response.data = data;
+            return res.send(response);
+        })
+        .catch((error) => {
+            const response = Response.errorResponse();
+            response.error.message = error.message;
+            response.error.code = 400;
+            return res.status(400).send(response); //quitar status??
+        });
+    },
+
+    pagination: async (req, res) => {
+        const { limit = 5, skip = 0 } = req.query;
+        //console.log(req.user) una vez que inician sesion ya se obtiene los datos del usuario con req.user
+        Personal.Pagination(limit, skip)
+        .then((data) => {
+            const response = Response.successResponse();
+            response.data = data;
+            return res.send(response);
+        })
+        .catch((error) => {
+            const response = Response.errorResponse();
+            response.error.message = error.message;
+            response.error.code = 400;
+            return res.status(400).send(response); //quitar status??
+        });
+    },
+    
+    deactivate: async (req, res) => {
+        const id_personal = req.params.id_personal;
+        Personal.Deactivate(id_personal)
+        .then((data) => {
+            const response = Response.successResponse();
+            response.data = data;
+            return res.send(response);
+        })
+        .catch((error) => {
+            const response = Response.errorResponse();
+            response.error.message = error.message;
+            response.error.code = 400;
+            return res.status(400).send(response); //quitar status??
+        });
+    },
+
+    activate: async (req, res) => {
+        const id_personal = req.params.id_personal;
+        Personal.Activate(id_personal)
+        .then((data) => {
+            const response = Response.successResponse();
+            response.data = data;
+            return res.send(response);
+        })
+        .catch((error) => {
+            const response = Response.errorResponse();
+            response.error.message = error.message;
+            response.error.code = 400;
+            return res.status(400).send(response); //quitar status??
+        });
+    },
 };
-
